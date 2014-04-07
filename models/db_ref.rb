9da2525077a2a801d82cc21ab0e7688107ca8a0b
Cassiano D'Andrea <cassiano.dandrea@tagview.com.br>
2014-04-07 13:21:23 -0300

    Allowing the repository to be in "detached head" state (when it contains a SHA1 directly)

--- models/db_ref.rb
+++ models/db_ref.rb
@@ -4,6 +4,10 @@ class DbRef < ActiveRecord::Base
   def self.sha1_referenced_by(name)
     head_ref = find_by_name(name)
 
-    DbBranch.find_by_name(head_ref.ref).sha1
+    if head_ref.ref =~ /\A\h{40}\Z/       # Is is a SHA1?
+      head_ref.ref
+    else                                  # No, so look for a branch with that name.
+      (branch = DbBranch.find_by_name(head_ref.ref)) && branch.sha1
+    end
   end
 end
