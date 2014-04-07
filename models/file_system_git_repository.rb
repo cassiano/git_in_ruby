6a46a7546a81b0dd6f9058c1c49de70439b0de21
Cassiano D'Andrea <cassiano.dandrea@tagview.com.br>
2014-04-07 00:32:12 -0300

    Add method for calculation time offset

--- models/file_system_git_repository.rb
+++ models/file_system_git_repository.rb
@@ -32,11 +32,15 @@ class FileSystemGitRepository < GitRepository
   end
 
   def format_commit_data(tree_sha1, parents_sha1, author, committer, subject)
+    current_time                 = Time.now
+    current_time_seconds_elapsed = current_time.to_i                                  # Seconds elapsed since 01/Jan/1970 00:00:00.
+    current_time_utc_offset      = time_offset_for_commit(current_time.utc_offset)    # Should range from '-2359' to '+2359'.
+
     data = ""
     data << "tree #{tree_sha1}\n"
     data << parents_sha1.map { |sha1| "parent #{sha1}\n" }.join
-    data << "author #{author} #{Time.now.to_i} -0300\n"
-    data << "committer #{committer} #{Time.now.to_i} -0300\n"
+    data << "author #{author} #{current_time_seconds_elapsed} #{current_time_utc_offset}\n"
+    data << "committer #{committer} #{current_time_seconds_elapsed} #{current_time_utc_offset}\n"
     data << "\n"
     data << subject + "\n"
   end
@@ -136,5 +140,13 @@ class FileSystemGitRepository < GitRepository
     raise MissingCommitDataError, "Missing subject in commit." unless (empty_row_index = rows.index(''))
 
     rows[empty_row_index+1..-1].join("\n")
+  end
+
+  def time_offset_for_commit(seconds)
+    sign = seconds < 0 ? '-' : '+'
+    hour   = seconds.abs / 3600
+    minute = (seconds.abs - hour * 3600) / 60
+
+    '%1s%02d%02d' % [sign, hour, minute]
   end
 end
