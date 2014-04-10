6090eaddb9d67551c01c4f8fba31f27a13200e62
Cassiano D'Andrea <cassiano.dandrea@tagview.com.br>
2014-04-10 00:28:54 -0300

    Removing incorrect index

--- db/migrate/004_create_db_tree_entries.rb
+++ db/migrate/004_create_db_tree_entries.rb
@@ -11,8 +11,7 @@ class CreateDbTreeEntries < ActiveRecord::Migration
     add_foreign_key :db_tree_entries, :db_objects, column: :tree_id
     add_foreign_key :db_tree_entries, :db_objects, column: :git_object_id
 
-    add_index :db_tree_entries, [:tree_id, :git_object_id], :unique => true
-    add_index :db_tree_entries, [:tree_id, :name],          :unique => true
+    add_index :db_tree_entries, [:tree_id, :name], :unique => true
   end
 
   def self.down
