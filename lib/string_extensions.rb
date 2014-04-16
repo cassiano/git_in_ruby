88886000d5a5843c2fae390a2f7ff24358f8f544
Cassiano D'Andrea <cassiano.dandrea@tagview.com.br>
2014-04-16 09:45:44 -0300

    Minor refactoring

--- lib/string_extensions.rb
+++ lib/string_extensions.rb
@@ -15,7 +15,8 @@ class String
   end
 
   def find_valid_encoding
-    encodings = (['US-ASCII', 'UTF-8', 'ISO-8859-1'] + Encoding.aliases.values - ["ASCII-8BIT"]).uniq + ["ASCII-8BIT"]
+    # List all possible Ruby encodings, starting from the most restrictive (US-ASCII) and ending with the least (BINARY = ASCII-8BIT).
+    encodings = (%w(US-ASCII UTF-8 ISO-8859-1) + Encoding.aliases.values - ['ASCII-8BIT']).uniq + ['ASCII-8BIT']
 
     encodings.find do |encoding|
       self.dup.force_encoding(encoding).valid_encoding?
